db5e06bc3c6c79dc6ff4484e35bdb9dba9d9c087
Day-void <maringisanwaday@gmail.com>
2026-06-29 20:39:59 +0200

    Update on my exercises

--- loops.js
+++ loops.js
@@ -1,23 +1,34 @@
-// 1. Print every number in an array
+// // 1. Print every number in an array
 
-const numbers = [5, 10, 15, 20, 25];
+// const numbers = [5, 10, 15, 20, 25];
+
+// numbers.forEach(num => {
+//     console.log(num);
+// });
+
+// // 2. Print every name in an array
+
+// const names = ["Alice", "Bob", "Charlie", "David"];
+
+// names.forEach(name => {
+//     console.log(name);
+// });
+
+// // 3. Print each fruit in uppercase
+
+// const fruits = ["apple", "banana", "orange"];
+
+// fruits.forEach(fruit => {
+//     console.log(fruit.toUpperCase());
+// });
+
+// 4. Find the sum of all numbers
+
+const numbers = [2, 4, 6, 8, 10];
+let sum = 0;
 
 numbers.forEach(num => {
-    console.log(num);
+    sum += num;
 });
 
-// 2. Print every name in an array
-
-const names = ["Alice", "Bob", "Charlie", "David"];
-
-names.forEach(name => {
-    console.log(name);
-});
-
-// 3. Print each fruit in uppercase
-
-const fruits = ["apple", "banana", "orange"];
-
-fruits.forEach(fruit => {
-    console.log(fruit.toUpperCase());
-});
+console.log("Sum:", sum);
