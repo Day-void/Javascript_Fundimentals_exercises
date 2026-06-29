410bc33b59bf463099a6e749efc43eb3b130526a
Day-void <maringisanwaday@gmail.com>
2026-06-29 20:35:57 +0200

    second question

--- loops.js
+++ loops.js
@@ -5,3 +5,11 @@ const numbers = [5, 10, 15, 20, 25];
 numbers.forEach(num => {
     console.log(num);
 });
+
+// 2. Print every name in an array
+
+const names = ["Alice", "Bob", "Charlie", "David"];
+
+names.forEach(name => {
+    console.log(name);
+});
